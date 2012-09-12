e3b85a81b132ff54a992968b42da899d48ddc3ac
jschr <jordan.schroter@gmail.com>
2012-09-12 16:38:06 -0300

    Update jquery.google.fastbutton.js

--- jquery.google.fastbutton.js
+++ jquery.google.fastbutton.js
@@ -1,8 +1,24 @@
 (function($) {
-  $.fn.fastClick = function(handler) {
-    return $(this).each(function() {
-      new FastButton($(this)[0], handler);
-    });
+  $.event.special.fastClick = {
+    setup: function () {
+        $(this).data('fastClick', new FastButton(this, $.event.special.fastClick.handler));
+    },
+    teardown: function () {
+       $(this).data('fastClick').destroy();
+    },
+    handler: function (e) {
+  		// convert native event to jquery event
+  		e = $.event.fix(e);
+  		e.type = 'fastClick';
+  
+  		$.event.handle.apply(this, arguments);
+    }
   };
+
+	$.fn.fastClick = function(fn) {
+		return $(this).each(function() {
+			return fn ? $(this).bind("fastClick", fn) : $(this).trigger("fastClick");
+		});
+	};
 }(jQuery));
 
