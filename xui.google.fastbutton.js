017c9bcd9aa5bfb90cee70f047b684edceda48de
alexsomeoddpilot <alex@someoddpilot.com>
2014-05-28 14:02:15 -0500

    Adding "use strict" to xui implementation

--- xui.google.fastbutton.js
+++ xui.google.fastbutton.js
@@ -1,5 +1,7 @@
 /** Integrate google.fastbutton.js with XUI */
 (function(x$) {
+  "use strict";
+
   x$.fn.fastClick = function(handler, useCapture) {
     return this.each(function(element, index, xui) {
       element.fastButton = new FastButton(xui[index], handler, useCapture);
